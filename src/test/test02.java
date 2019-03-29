b1004801581ebb9ce8ca25f3d29842c0f2458731
流小量 <Liu@186.com>
2019-03-29 19:30:11 +0800

    test02

---
+++ src/test/test02.java
@@ -0,0 +1,34 @@
+package test;
+
+public class test02 {
+    public static void main(String[] args) {
+        System.out.println("hhhhhhhhhhh");
+        System.out.println("hhhhhhhhhhh");
+        System.out.println("hhhhhhhhhhh");
+        System.out.println("hhhhhhhhhhh");
+        System.out.println("hhhhhhhhhhh");
+        System.out.println("aaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaa");
+        System.out.println("hhhhhhhhhhh");
+        System.out.println("hhhhhhhhhhh");
+    }
+}
